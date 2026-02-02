298761a78a8fb30b686a3534189a9dd7267e32a7
Chen <chen.dong@tmx.com>
2026-02-01 18:27:11 -0800

    update telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,20 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+    local actions = require("telescope.actions")
+
     require('telescope').setup {
       defaults = {
         mappings = {
-          i = { -- Insert mode
-            ["<C-j>"] = require("telescope.actions").move_selection_next,
-            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-            ["<C-h>"] = require("telescope.actions").cycle_history_prev,
-            ["<C-l>"] = require("telescope.actions").cycle_history_next,
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-h>"] = actions.cycle_history_prev,
+            ["<C-l>"] = actions.cycle_history_next,
           },
-          n = { -- Normal mode
-            ["j"] = require("telescope.actions").move_selection_next,
-            ["k"] = require("telescope.actions").move_selection_previous,
-            ["h"] = require("telescope.actions").cycle_history_prev,
-            ["l"] = require("telescope.actions").cycle_history_next,
+          n = {
+            ["j"] = actions.move_selection_next,
+            ["k"] = actions.move_selection_previous,
           },
         },
         vimgrep_arguments = {
@@ -27,18 +27,28 @@ return {
           "--column",
           "--smart-case",
         },
+        -- FIX: Removed preview_width from the root of layout_config
         layout_config = {
-          horizontal = { width = 0.8, height = 0.9, preview_width = 0.5 },
-          preview_width = 0.5,       -- Reduce preview size
+          horizontal = {
+            width = 0.8,
+            height = 0.9,
+            preview_width = 0.5
+          },
+          vertical = {
+            mirror = false,
+          },
         },
-        sorting_strategy = "ascending", -- Show results in ascending order
-        dynamic_preview_title = true,   -- Dynamically update preview title
-        prompt_prefix = " ",
-        selection_caret = " ",
         layout_strategy = "horizontal",
+        sorting_strategy = "ascending",
+        dynamic_preview_title = true,
+        -- Updated icons for Nerd Font V3
+        prompt_prefix = "   ",
+        selection_caret = "󰄾 ",
+        entry_prefix = "  ",
       },
       pickers = {
         find_files = {
+          -- Dropdown handles its own layout, so it won't crash now
           theme = "dropdown",
           previewer = false,
         },
@@ -53,7 +63,7 @@ return {
           override_file_sorter = true,
         },
         live_grep_args = {
-          auto_quoting = true, -- Dynamically quote strings with spaces
+          auto_quoting = true,
         },
       },
     }
